d57fd808d994e9a6d44df640ee06cf1c42b8e944
Elliott Cost <email@elliott.computer>
2024-03-13 12:57:57 +0100

    version

--- site/snippets/meta.php
+++ site/snippets/meta.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= css('assets/css/fonts.css?v=1.0') ?>
-    <?= css('assets/css/global.css?v=1.0') ?>
+    <?= css('assets/css/fonts.css?v=1.6') ?>
+    <?= css('assets/css/global.css?v=1.6') ?>
     <?php
     if(page()->template() == 'home') {
-        echo css('assets/css/home.css?v=1.5');
+        echo css('assets/css/home.css?v=1.6');
     }
     ?>
     <title><?= $site->title() ?></title>
